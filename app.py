4140848022e9ba5aedf2c72bda9367bc00fd4da4
Ewa <ewa.sierka96@gmail.com>
2024-01-17 17:03:10 +0100

    commit

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ from PyQt5.QtGui import QPixmap
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtGui import QCursor
 
+widgets = {
+    "logo": [],
+    "button": [],
+    "score": [],
+    "question": [],
+    "answer1": [],
+    "answer2": [],
+    "answer3": [],
+    "answer4": []
+}
+
 app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle("Calculate your loan")
@@ -13,14 +24,107 @@ window.setStyleSheet("background: #161219;")
 
 grid = QGridLayout()
 
-image = QPixmap("logo.png")
-logo = QLabel()
-logo.setPixmap(image)
-# logo.setAlignment(QtCore.Qt.Alignment)
-logo.setStyleSheet("margin-top: 100px;")
+def clear():
+    for widget in widgets:
+        if widgets[widget] != []:
+            widgets[widget][-1].hide()
+        for i in range(0, len(widgets[widget])):
+            widgets[widget].pop()
 
-grid.addWidget(logo, 0, 0)
+def start():
+    clear()
+    frame2()
 
+def create_buttons(answer, l_margin, r_margin):
+    button = QPushButton(answer)
+    button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+    button.setFixedWidth(485)
+    button.setStyleSheet(
+        "*{border: 4px solid '#BC006C';" +
+        "margin-left: " + str(l_margin) + "px;" +
+        "margin-right: " + str(r_margin) + "px;" +
+        "border-radius: 25px;" +
+        "font-size: 16px;" +
+        "color: 'white';" +
+        "font-family:'shanti';" +
+        "padding: 15px 0;" +
+        "margin-top: 20px}" +
+        "*:hover{background: '#BC006C'}"
+    )
+    return button
+
+def frame1():
+    image = QPixmap("logo.png")
+    logo = QLabel()
+    logo.setPixmap(image)
+    logo.setAlignment(QtCore.Qt.AlignCenter)
+    logo.setStyleSheet("margin-top: 100px;")
+    widgets["logo"].append(logo)
+
+    #button
+
+    button = QPushButton("Calculate")
+    button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+    button.setStyleSheet(
+        "*{border: 4px solid '#BC006C';" +
+        "border-radius: 15px;" +
+        "font-size: 35px;" +
+        "color: 'white';" +
+        "padding: 25x 0;" +
+        "margin: 100px 200px;}" +
+        "*:hover{background: '#BC006C';}"
+    )
+    button.clicked.connect(start)
+    widgets["button"].append(button)
+
+    grid.addWidget(widgets["logo"][-1], 0, 0)
+    grid.addWidget(widgets["button"][-1], 1, 0)
+
+def frame2():
+    score = QLabel("80")
+    score.setAlignment(QtCore.Qt.AlignRight)
+    score.setStyleSheet(
+        '''
+        font-size: 35px;
+        color: 'white';
+        padding: 25px 20px 0px 20px;
+        margin: 20px 200px;
+        background: '#64A314';
+        border: 1px solid '#64A314';
+        border-radius: 45px;
+        '''
+    )
+    widgets["score"].append(score)
+
+    # question widget
+    question = QLabel("Placeholder text will go here blah blah")
+    question.setAlignment(QtCore.Qt.AlignCenter)
+    question.setWordWrap(True)
+    question.setStyleSheet(
+        "font-family: Shanti;" +
+        "font-size: 25px;" +
+        "color: 'white';" +
+        "padding: 75px;"
+    )
+    widgets["question"].append(question)
+
+    button1 = create_buttons("answer1", 85, 5)
+    button2 = create_buttons("answer2", 5, 85)
+    button3 = create_buttons("answer3", 85, 5)
+    button4 = create_buttons("answer4", 5, 85)
+    widgets["answer1"].append(button1)
+    widgets["answer2"].append(button2)
+    widgets["answer3"].append(button3)
+    widgets["answer4"].append(button4)
+
+    grid.addWidget(widgets["score"][-1], 0, 1)
+    grid.addWidget(widgets["question"][-1], 1, 0, 1, 2)
+    grid.addWidget(widgets["answer1"][-1], 2, 0)
+    grid.addWidget(widgets["answer2"][-1], 2, 1)
+    grid.addWidget(widgets["answer3"][-1], 3, 0)
+    grid.addWidget(widgets["answer4"][-1], 3, 1)
+
+frame1()
 
 window.setLayout(grid)
 
