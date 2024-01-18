9918aa3ac0c72fc593b578e1dd487ad7772dc659
Ewa <ewa.sierka96@gmail.com>
2024-01-18 22:50:37 +0100

    commit

--- app2.py
+++ app2.py
@@ -1,4 +1,7 @@
 import sys
+import fitz
+import os
+import re
 from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtGui, QtCore, QtQuick
@@ -16,6 +19,21 @@ widgets = {
     "answer4": []
 }
 
+
+#Szczególy raty
+pattern1 = re.compile(r"(Spłata raty\n)([0-9\s,].*)(PLN)([0-9\s,].*)(PLN)([0-9\s,].*)(PLN)")
+#Nadpłaty
+pattern2 = re.compile(r"(Spłata raty pozaplanowej\n)([0-9\s,].*)(PLN)")
+#Harmonogram
+pattern3 = re.compile(r"(Płatność\n)([0-9\s,].*)(\n?PLN)([0-9\s,].*)(\n?PLN)([0-9\s,].*)(\n?PLN)([0-9\s,].*)")
+raty = []
+kapital = []
+odsetki = []
+nadplaty = []
+przyszla_rata = []
+przyszly_kapital = []
+przyszle_odsetki = []
+
 app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle("Calculate your loan")
@@ -28,15 +46,51 @@ grid = QGridLayout()
 class App(QWidget):
     def browse(self):
         dialog = QFileDialog()
-        dialog.setDirectory(r'C:\Users\ewasi\PycharmProjects\CFG\ING')
+        self.path = r'C:\Users\ewasi\PycharmProjects\CFG\ING'
+        dialog.setDirectory(self.path)
         dialog.setNameFilter("Documents (*.pdf)")
         dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
         dialog.setViewMode(QFileDialog.ViewMode.List)
         if dialog.exec():
-            filename = dialog.selectedFiles()
-        print(filename[-1])
+            self.filename = dialog.selectedFiles()
+        print(self.filename)
         # print(type(filename[-1]))
-        self.label.setText(filename[-1])
+        # self.label.setText(self.filename[-1])
+        self.read_file()
+
+    def read_file(self):
+        for i in self.filename:
+            doc = fitz.open(i)
+            text = ""
+            for page in doc:
+                text += page.get_text()
+            for item in pattern1.finditer(text):
+                raty.append(float(item.group(2).replace(u'\xa0', '').replace(',', '.')))
+                kapital.append(float(item.group(4).replace(u'\xa0', '').replace(',', '.')))
+                odsetki.append(float(item.group(6).replace(u'\xa0', '').replace(',', '.')))
+            for item in pattern2.finditer(text):
+                nadplaty.append(float(item.group(2).replace(u'\xa0', '').replace(',', '.')))
+
+        suma_kapital = 0
+        del kapital[10]  # zwielokrotnione
+        for x in kapital:
+            suma_kapital = suma_kapital + x
+
+        suma_odsetki = 0
+        del odsetki[10]  # zwielokrotnione
+        for x in odsetki:
+            suma_odsetki = suma_odsetki + x
+
+        suma_nadplaty = 0
+        for x in nadplaty:
+            suma_nadplaty = suma_nadplaty + x
+
+        print(f'Historia:\nKapitał: {round(suma_kapital, 2)}')
+        print(f'Nadpłaty: {suma_nadplaty}')
+        print(f'Suma: {suma_kapital + suma_nadplaty}')
+        print(f'Pozostało: {round(160000 - (suma_kapital + suma_nadplaty), 2)}')
+        print(f'Odsetki: {round(suma_odsetki, 2)}')
+
 
     def main_button(self, text):
         button = QPushButton(text)
